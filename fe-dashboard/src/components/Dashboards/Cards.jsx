595ec747163c0c9017a7c1e3222f5f0658948d73
ishala <faishalali22@students.amikom.ac.id>
2026-02-28 20:03:10 +0700

    refactor for modularize cards component

---
+++ fe-dashboard/src/components/Dashboards/Cards.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import StatCard from "./StatCard";
+import { cardsInfo } from "../../utils";
+
+function Cards({ topGapGenre, summary, genreData, isSales }) {
+  const cards = cardsInfo(topGapGenre, summary, genreData, isSales)
+  return (
+    <div className="grid grid-cols-2 xl:grid-cols-4 gap-5">
+      {cards.map((c) => (
+        <StatCard key={c.label} {...c} />
+      ))}
+    </div>
+  );
+}
+
+Cards.propTypes = {
+  summary: PropTypes.shape({
+    total_games:      PropTypes.number,
+    avg_global_price: PropTypes.number,
+    total_sales:      PropTypes.number,
+    avg_our_price:    PropTypes.number,
+  }).isRequired,
+  genreData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  topGapGenre: PropTypes.arrayOf(PropTypes.object).isRequired,
+  isSales: PropTypes.bool.isRequired
+};
+
+export default Cards;
