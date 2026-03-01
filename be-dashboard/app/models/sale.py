64a764e60b56ec0221d5623a7aa2c7459c58c82e
ishala <faishalali22@students.amikom.ac.id>
2026-03-01 13:49:30 +0700

    delete unused import

--- be-dashboard/app/models/sale.py
+++ be-dashboard/app/models/sale.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, Text, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.db.database import Base
@@ -8,8 +8,8 @@ class Sale(Base):
     __tablename__ = "sales"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    game_id = Column(Integer, ForeignKey("games.id"), nullable=False)  # wajib referensi ke game
-    our_price = Column(Float, nullable=False)          # harga jual di toko sendiri
+    game_id = Column(Integer, ForeignKey("games.id"), nullable=False)
+    our_price = Column(Float, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=True)
 
